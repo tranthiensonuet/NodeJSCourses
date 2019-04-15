29fdbda2d630f4677a8ff98e43e7e5ba8e28b885
SonThien <tranthienson1999@gmail.com>
2019-04-15 22:24:26 +0700

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 const http = require ('http');
+const url = require ('url');
+const port = process.env.PORT;
+const host = process.env.HOST;
 const server = http.createServer(function(req, res){
     if (req.method == "GET" && req.url == '/hello'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
         res.write('world');
         res.end();
     }
     else if (req.method == 'POST' && req.url == '/hello'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
         res.write('world created');
         res.end();
     }
     else if (req.method == 'PUT' && req.url == '/hello'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
         res.write('world updated');
         res.end();
     }
     else if (req.method == 'DELETE' && req.url == '/hello'){
+        res.writeHead(200, {"Content-Type": "text/plain"});
         res.write('world deleted');
+        res.end();
+    }
+    else{
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write('URL error');
         res.end();
     }
 });
